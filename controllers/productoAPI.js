3aaaae249275b7307fb25022a1bed5659afc45c2
carconex <karmeconemar@gmail.com>
2024-11-23 11:18:35 +0100

    xicotet canvi

--- controllers/productoAPI.js
+++ controllers/productoAPI.js
@@ -68,8 +68,8 @@ exports.postProductos = async(req, res) => {
         const { name, desc, _idTipo, price } = req.body;
 
         // xicoteta comprobació abans d'aguardar la info
-        if (!name || !price || !desc) { 
-            res.status(400).send('Falten campss requerits'); 
+        if (!name || !price || !desc || _idTipo) { 
+            res.status(400).send('Falten campss requerits' + name + price + desc + _idTipo); 
             return; 
         }
 
