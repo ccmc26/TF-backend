700c235f6f7de8cc73b6ca2b9242f55e89f9d5da
carconex <karmeconemar@gmail.com>
2024-11-23 11:30:47 +0100

    canviant una coseta del cors a vore si va el metodo post en la url del deploy

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ mongoose.connect(mongoURI)
 
 // middleware per a que la app puga analitzar el body d'una sol·licitud
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:8080", "https://tf-backend-production.up.railway.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials:true
+}));
 setupRoutes(app);
 
 app.listen(PORT, () => {
